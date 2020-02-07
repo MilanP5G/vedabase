5debc917c5c300cb4abb0d27b9be4df0fcf63047
MilanP5G <milan_parmar@hotmail.co.uk>
2020-02-07 19:52:13 +0000

    Added a false CLI layout to test interface structure

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,22 @@
 class Vedabase::CLI
 
   def ready
-    puts "Welcome to the Vedabase! An introduction into Śrīla Prabhupāda's books!"
+   list_books
+   choice
   end
+
+  def list_books
+    puts "Welcome to the Vedabase! An introduction into a few of Śrīla Prabhupāda's most popular books!"
+    puts <<-DOC
+      1. Bhagavad-Gītā
+      2. Śrīmad-Bhāgavatam
+      3. Śrī Caitanya-Caritāmṛta
+      DOC
+    end
+
+    def choice
+      puts "Enter the number of the book you would like to know more about:"
+    end
 
 
 end
