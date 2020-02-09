f22dc89e8441981be35cf5234917c1e5e1636c3d
MilanP5G <milan_parmar@hotmail.co.uk>
2020-02-09 17:51:51 +0000

    created initialize method and .all method in vedabase class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,24 +2,7 @@ require_relative './vedabase.rb'
 
 class Vedabase::Scraper
 
-#
-# def get_page
-#   doc = Nokogiri::HTML(open(https://vedabase.io/en))
-# end
-#
-# def get_books
-#   self.get_page.css("div.mm-0")
-# end
-#
-# def make_books
-#   self.get_books.each do | book |
-#     books = Vedabase.new
-#     books.title = book.css("h1").text
-#     books.intro = book.css("div.col-12").text
-#   end
-# end
-def self.scrape_books
-
+def self.scrape_book
   doc = Nokogiri::HTML(open("https://vedabase.io/en/"))
 
     books = doc.css("div.mm-0")
--- lib/vedabase.rb
+++ lib/vedabase.rb
@@ -1,12 +1,16 @@
 class Vedabase::Vedabase
-  # attr_accessor :title, :introduction
-  #
-  # @@all = []
-  #
-  # def initialize
-  #   @title = title
-  #   @introduction = introduction
-  #   @@all << self
-  # end
+  attr_accessor :title, :introduction
+
+  @@all = []
+
+  def initialize
+    @title = title
+    @introduction = introduction
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
 
 end
