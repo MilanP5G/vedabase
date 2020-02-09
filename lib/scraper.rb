488182d709caffc49671f94b98e8356d983b56e9
MilanP5G <milan_parmar@hotmail.co.uk>
2020-02-09 14:35:28 +0000

    created methods within scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,22 @@
+require_relative './vedabase.rb'
+
 class Vedabase::Scraper
 
 
+def get_page
+  Nokogiri::HTML(open(https://vedabase.io/en/))
+end
 
-end 
+def get_books
+  self.get_page.css("div.mm-0")
+end
+
+def make_books
+  self.get_books.each do | book |
+    books = Vedabase.new
+    books.title = book.css("h1").text
+    books.intro = book.css("div.col-12").text
+  end
+end
+
+end
